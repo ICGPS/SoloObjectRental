7b7ae90d2fc34cd38200158aff5cebebcbfeef41
NARA <skfk6788@naver.com>
2024-01-14 18:23:32 +0900

    페이지 타이틀, css 링크 추가

--- src/main/java/org/choongang/cs/controllers/CsController.java
+++ src/main/java/org/choongang/cs/controllers/CsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller("csController")
 @RequestMapping("/cs")
@@ -20,7 +21,8 @@ public class CsController implements ExceptionProcessor {
 
     // 고객지원 홈
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        commonProcess("index", model);
 
         System.out.println("cs화면 테스트");
 
@@ -29,22 +31,44 @@ public class CsController implements ExceptionProcessor {
 
     // 1:1 문의
     @GetMapping("/inquiry")
-    public String inquiry() {
+    public String inquiry(Model model) {
+        commonProcess("inquiry", model);
 
         return utils.tpl("cs/inquiry");
     }
 
     @GetMapping("/inquiryAdd")
-    public String inquiryAdd() {
+    public String inquiryAdd(Model model) {
+        commonProcess("inquiryAdd", model);
 
         return utils.tpl("cs/inquiry_add");
     }
 
     // 칭찬/개선
     @GetMapping("/improvement")
-    public String improvement() {
+    public String improvement(Model model) {
+        commonProcess("improvement", model);
 
         return utils.tpl("cs/improvement");
     }
 
+    private void commonProcess(String mode, Model model) {
+        mode = Objects.requireNonNullElse(mode, "index");
+        String pageTitle = "고객지원";
+
+        List<String> addCss = new ArrayList<>();
+
+        if (mode.equals("inquiry") || mode.equals("inquiryAdd")) {
+            pageTitle = mode.equals("inquiry") ? "1:1 문의" : "1:1 문의 작성";
+
+        } else if (mode.equals("improvement")) {
+            pageTitle = "칭찬/개선";
+        }
+
+        addCss.add("cs/cs");
+
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("addCss", addCss);
+
+    }
 }
