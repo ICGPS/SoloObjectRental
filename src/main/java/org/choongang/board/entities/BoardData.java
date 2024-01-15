e70b6bb4dfb0cf9836b139b1dfc1a316ab77c169
kwi5124 <kwi5124@naver.com>
2024-01-15 17:53:23 +0900

    게시판

--- src/main/java/org/choongang/board/entities/BoardData.java
+++ src/main/java/org/choongang/board/entities/BoardData.java
@@ -1,16 +1,73 @@
 package org.choongang.board.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.choongang.commons.entities.Base;
 import org.choongang.commons.entities.BaseMember;
+import org.choongang.member.entities.Member;
+
+import java.util.UUID;
 
 @Data
 @Entity
-public class BoardData extends BaseMember {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(indexes = {@Index(name = "idx_boardData_basic", columnList = "notice DESC, createdAt DESC")})
+public class BoardData extends Base {
     @Id @GeneratedValue
     private Long seq;
-    private String subject;
-    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boardSeq")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberSeq")
+    private Member member;
+
+    @Column(length = 65, nullable = false)
+    private String gid = UUID.randomUUID().toString();
+
+    @Column(length = 40, nullable = false)
+    private String poster; //작성자
+
+    private String guestPw; //비회원 비밀번호
+
+    private boolean notice; //공지글 여부 - true : 공지글
+
+    @Column(nullable = false)
+    private String subject; //제목
+
+    @Lob
+    @Column(nullable = false)
+    private String content; //내용
+
+    private int viewCount; //조회수
+
+    @Column(length = 20)
+    private String ip; //IP주소
+
+    private String ua; //USER-agent : 브라우저 정보
+
+    private int num1; // 추가 필드 : 정수
+    private int num2;
+    private int num3;
+
+    @Column(length = 100)
+    private String text1; //추가 필드 : 한줄 텍스트
+    @Column(length = 100)
+    private String text2;
+    @Column(length = 100)
+    private String text3;
+
+    @Lob
+    private String longText1; //추가 필드 : 여러줄 텍스트
+    @Lob
+    private String longText2;
+    @Lob
+    private String longText3;
 }
