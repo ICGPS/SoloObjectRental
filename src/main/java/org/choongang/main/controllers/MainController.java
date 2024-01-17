3b4c2b09f781268932328cc4060304d02404e0fa
kwi5124 <kwi5124@naver.com>
2024-01-17 19:34:36 +0900

    메인

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -4,24 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.choongang.commons.ExceptionProcessor;
 import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController implements ExceptionProcessor {
 
-  private final Utils utils;
+    private final Utils utils;
 
-  @ModelAttribute("addCss")
-  public String[] addCss() {
+    @ModelAttribute("addCss")
+    public String[] addCss() {
+        return new String[] { "main/style" };
+    }
 
-    return new String[] {"main/style"};
-  }
+    @GetMapping("/")
+    public String index() {
 
-  public String index() {
-
-    return utils.tpl("main/index");
-  }
-
+        return utils.tpl("main/index");
+    }
 }
+
