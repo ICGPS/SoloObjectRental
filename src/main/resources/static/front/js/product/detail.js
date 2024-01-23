f37f60978b0310126c24d17c2f3ab825aecda765
allely <ggrocky49@gmail.com>
2024-01-23 17:47:53 +0900

    상품리스트 css, 상품상세 css 하단바, js

--- src/main/resources/static/front/js/product/detail.js
+++ src/main/resources/static/front/js/product/detail.js
@@ -38,17 +38,25 @@ window.addEventListener("DOMContentLoaded", function() {
     /* 상품 메인 썸네일 이벤트 처리 E */
 
     /* 상품 수량 증가, 감소 처리 S */
+    // 버튼 클릭 시 증감
     const changeEaEls = document.querySelectorAll(".selected_products .change_ea");
     for (const el of changeEaEls) {
         el.addEventListener("click", productDetails.changeEa);
     }
 
+    // 상품 갯수 변화(직접입력)시 상단 하단 바 연동.
+    // 버튼으로 변경시 즉시 적용 X ( input 창 클릭 후 외부 클릭(blur) 시 적용 )
     const eaEls = document.querySelectorAll(".selected_products input[type='number']");
     for (const el of eaEls) {
         el.addEventListener("blur", function() {
             let ea = parseInt(this.value);
             ea = ea < 1 ? 1 : ea;
             this.value = ea;
+            for (const otherEl of eaEls) {
+                if (otherEl !== this) {
+                otherEl.value = ea;
+                }
+            }
         });
     }
     /* 상품 수량 증가, 감소 처리 E */
@@ -67,4 +75,29 @@ window.addEventListener("DOMContentLoaded", function() {
         });
     }
     /** 찜하기, 장바구니, 주문하기 버튼 처리 E */
+
+
+    /* 상단 벗어날 시 하단 바 생성 S */
+    // 상단 메인 영역
+    var product_main = document.getElementById('product_main');
+    // 메인 영역 지날 시 보일 하단 바
+    var bottomBar = document.getElementById('bottomBar');
+
+    window.addEventListener("scroll", function () {
+        // product_main 섹션의 위치 정보 가져오기
+        var productMainRect = product_main.getBoundingClientRect();
+
+        // product_main 섹션이 화면에 보일 때와 사라질 때 각각 처리
+        if (productMainRect.top <= window.innerHeight && productMainRect.bottom >= 0) {
+
+            bottomBar.style.visibility='hidden';
+
+        } else if (productMainRect.bottom <= 0) {
+
+            bottomBar.style.visibility='visible';
+
+        }
+    });
+    /* 상단 벗어날 시 하단 바 생성 E */
+
 });
