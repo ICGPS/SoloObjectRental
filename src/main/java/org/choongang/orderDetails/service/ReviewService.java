ffd0f2b340c3b4802e8a8ce69733804ed0dbf51b
narrrra <skfk6788@naver.com>
2024-01-23 17:28:04 +0900

    후기 작업 중

--- src/main/java/org/choongang/orderDetails/service/ReviewService.java
+++ src/main/java/org/choongang/orderDetails/service/ReviewService.java
@@ -1,17 +1,22 @@
 package org.choongang.orderDetails.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.cs.controllers.RecordInquiry;
+import org.choongang.cs.entities.Inquiry;
 import org.choongang.member.MemberUtil;
 import org.choongang.member.entities.Member;
 import org.choongang.member.service.MemberInfo;
 import org.choongang.orderDetails.entities.Review;
 import org.choongang.orderDetails.repositories.ReviewRepository;
 import org.choongang.product.entities.Product;
+import org.choongang.product.repositories.ProductRepository;
 import org.choongang.product.service.ProductInfoService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,11 +31,7 @@ public class ReviewService {
         Member member = memberUtil.getMember();
         review.setMemberNumber(member.getSeq());
 
-
-
         reviewRepository.saveAndFlush(review);
     }
-
-
 
 }
