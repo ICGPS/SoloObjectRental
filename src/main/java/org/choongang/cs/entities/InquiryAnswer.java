8a57da59d0b7409ad69e6eb92f4b5a0e4b5c4c74
narrrra <skfk6788@naver.com>
2024-01-29 12:07:19 +0900

    1:1 문의 답변 완료

--- src/main/java/org/choongang/cs/entities/InquiryAnswer.java
+++ src/main/java/org/choongang/cs/entities/InquiryAnswer.java
@@ -1,8 +1,6 @@
 package org.choongang.cs.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.choongang.commons.entities.BaseMember;
 
@@ -12,6 +10,8 @@ public class InquiryAnswer extends BaseMember {
     @Id
     @GeneratedValue
     private Long answerSeq;
+
+    @Column(unique = true)
     private Long inquirySeq; // 문의사항 번호
     private String answerContent;
 }
