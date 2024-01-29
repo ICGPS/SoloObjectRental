a8d1c1932102849f830fb5a8464dd3618821c567
SongHyunmsoo <y85858@naver.com>
2024-01-29 19:52:06 +0900

    배송지 관리 팝업 구현중 팝업창 내용 넣기

---
+++ src/main/java/org/choongang/member/entities/DeliveryList.java
@@ -0,0 +1,28 @@
+package org.choongang.member.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.choongang.commons.entities.Base;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+public class DeliveryList extends Base {
+    @Id
+    @GeneratedValue
+    private Long seq;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="memberSeq")
+    private Member member;
+
+    private boolean defaultAddress;
+
+    private String zonecode;
+    private String address;
+    private String addressSub;
+}
